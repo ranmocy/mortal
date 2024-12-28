55a157ba632a2f7693777fa65e0291d63401cdc0
Ranmocy <ranmocy@gmail.com>
2024-12-27 20:58:57 -0800

    Cleanup

--- Mortal/StatusMenuController.swift
+++ Mortal/StatusMenuController.swift
@@ -3,7 +3,7 @@
 //  Mortal
 //
 //  Created by Mocy Sheng on 4/26/15.
-//  Copyright (c) 2024 Mocy Sheng. All rights reserved.
+//  Copyright (c) 2015-2024 Mocy Sheng. All rights reserved.
 //
 
 import Cocoa
--- MortalTests/MortalTests.swift
+++ MortalTests/MortalTests.swift
@@ -2,8 +2,8 @@
 //  MortalTests.swift
 //  MortalTests
 //
-//  Created by Wanzhang Sheng on 4/24/15.
-//  Copyright (c) 2015 Wanzhang Sheng. All rights reserved.
+//  Created by Mocy Sheng on 4/24/15.
+//  Copyright (c) 2015-2024 Mocy Sheng. All rights reserved.
 //
 
 import Cocoa
